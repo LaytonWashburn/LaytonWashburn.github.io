8021e95ea1ff2ee566d7305ba34908f4211da3a2
Layton Washburn <layton.washburn1659@gmail.com>
2023-12-20 15:10:55 -0700

    Added a loading spinner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
 import { Navbar } from "./Navbar"
+import "../styles.css"
 
 export function Home(){
 
     return(
-        <div>
+        <div className="Home">
             <Navbar/>
             <p className="text">
                 Welcome to my skills website. Here I will layout my resume, classes, and projects
             </p>
+            <div class="circleSpinner">
+                <span class="loading">LOADING</span>
+            </div>  
         </div>
     )
 }
