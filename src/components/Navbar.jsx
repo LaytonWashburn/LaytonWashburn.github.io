aacd725342c1c7e14bfde9bd3b87f70eabedf429
Layton Washburn <layton.washburn1659@gmail.com>
2023-12-19 20:07:59 -0700

    Starting

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+export function Navbar(){
+
+    return(
+        <nav>
+            <Link>Resume</Link>
+            <Link>Classes</Link>
+            <Link>Projects</Link>
+        </nav>
+    )
+}
